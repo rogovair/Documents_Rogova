b8c85636ea6a4c2d3b1c203bca3b8602607d5a05
rogovair <irina_rogova_2004@mail.ru>
2024-02-12 08:57:34 +0300

    Добавьте файлы проекта.

---
+++ Documents_Rogova/LoginForm.cs
@@ -0,0 +1,70 @@
+﻿using Documents_Rogova;
+using Npgsql;
+using System;
+using System.Windows.Forms;
+
+namespace Documents_Rogova
+{
+    public partial class LoginForm : Form
+    {
+        private PostgresConnector bd = new PostgresConnector();
+        private int loginAttempts = 0;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLog.Text;
+            string password = textBoxPass.Text;
+
+            NpgsqlCommand cmd_ex = new NpgsqlCommand("SELECT COUNT(*) FROM users " +
+                "WHERE login = @login and password = @password", bd.getConnection());
+
+            cmd_ex.Parameters.AddWithValue("@login", login);
+            cmd_ex.Parameters.AddWithValue("@password", password);
+
+            bd.openConnection();
+
+            int user = Convert.ToInt32(cmd_ex.ExecuteScalar());
+
+            bd.closeConnection();
+
+            if (user > 0)
+            {
+                this.Hide();
+                MenuForm f1 = new MenuForm(login);
+                f1.ShowDialog();
+            }
+            else
+            {
+                loginAttempts++;
+
+                if (loginAttempts >= 3)
+                {
+                    DialogResult result = MessageBox.Show("Вы ввели неверный пароль 3 раза. Желаете сбросить пароль?", "Сброс пароля", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        using (ResetForm resetForm = new ResetForm(login))
+                        {
+                            DialogResult resetResult = resetForm.ShowDialog();
+                        }
+                        loginAttempts = 0;
+                    }
+                    else
+                    {
+                        ResetForm resetForm = new ResetForm(login);
+                        resetForm.ShowDialog();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неправильно введён логин или пароль.");
+                }
+            }
+        }
+    }
+}
---
+++ Documents_Rogova/ResetForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Documents_Rogova
+{
+    partial class ResetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNewPassword = new System.Windows.Forms.TextBox();
+            this.textBoxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNewPassword
+            // 
+            this.textBoxNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNewPassword.Location = new System.Drawing.Point(104, 146);
+            this.textBoxNewPassword.Name = "textBoxNewPassword";
+            this.textBoxNewPassword.Size = new System.Drawing.Size(183, 34);
+            this.textBoxNewPassword.TabIndex = 0;
+            // 
+            // textBoxConfirmPassword
+            // 
+            this.textBoxConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxConfirmPassword.Location = new System.Drawing.Point(104, 186);
+            this.textBoxConfirmPassword.Name = "textBoxConfirmPassword";
+            this.textBoxConfirmPassword.Size = new System.Drawing.Size(183, 34);
+            this.textBoxConfirmPassword.TabIndex = 1;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.ForeColor = System.Drawing.Color.White;
+            this.buttonReset.Location = new System.Drawing.Point(147, 226);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(93, 35);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Ок";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(104, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Поменяйте";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(393, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.textBoxConfirmPassword);
+            this.Controls.Add(this.textBoxNewPassword);
+            this.Name = "ResetForm";
+            this.Text = "ResetForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNewPassword;
+        private System.Windows.Forms.TextBox textBoxConfirmPassword;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+    }
+}
